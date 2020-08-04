46ed657b41c355bb04c770e7edfcf7abb9b657c0
Dborah <deborahferreirafonseca@gmail.com>
2020-08-04 19:53:49 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const $ = document.querySelector.bind(document)
+
+function TabNavigation(){
+
+    const html = {
+        links: [...$('.tab-links').children],//coloca os elementos em um array
+        contents:[...$('.tab-content').children],
+        opentab: $('[data-open]')
+    }
+    
+    function hideAllTabContent(){
+        html.contents.forEach(section => {
+            section.style.display = "none"
+        })
+    }
+    
+    function removeAllActiveClass(){
+        html.links.forEach(tab => {
+            tab.className = tab.className.replace("")
+        })
+    } 
+    
+    function showCurrentTab(id){
+        const tabcontent = $('#'+id)
+        tabcontent.style.display = 'block'
+    } 
+
+    function selectTab(event){
+        hideAllTabContent()
+        removeAllActiveClass()
+
+        const target = event.currentTarget
+        showCurrentTab(target.dataset.id)
+
+        target.className += "active"
+    }
+    
+    function listenForChange(){
+        html.links.forEach(tab => {
+            tab.addEventListener('click', selectTab )
+        })
+    } 
+    
+    function init(){
+        hideAllTabContent();
+        listenForChange();
+
+        html.opentab.click()
+    } 
+
+    return {
+        init
+    }
+}
+
+window.addEventListener('load', () => {
+    const tabNavigation = TabNavigation()
+    tabNavigation.init()
+})
+
+
+
+
